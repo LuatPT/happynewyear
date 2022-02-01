3458851d7b7e1551793481ba2a1ac8be63e1c797
luatpt <luatphamdhv@gmail.com>
2022-02-01 15:53:16 +0700

    Fix bug deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,29 +7,42 @@ import Button from '@material-ui/core/Button';
 
 const Home: NextPage = () => {
   useEffect(function onFirstMount(){
-   window?.addEventListener('click', function() { document?.getElementById('player').play() }, { once: true });
+    window?.addEventListener('click', function() { 
+      document?.getElementById('player').play() 
+    });
+    loadMessenger();
+    countVisitor();
   },[])
   function getGift(){
    alert("AHhh!");
   }
-
-  var chatbox = document.getElementById('fb-customer-chat');
+  function loadMessenger() {
+    var chatbox = document?.getElementById('fb-customer-chat');
       chatbox.setAttribute("page_id", "103999694475699");
       chatbox.setAttribute("attribution", "biz_inbox");
        window.fbAsyncInit = function() {
         FB.init({
-          xfbml            : true,
-          version          : 'v12.0'
+          xfbml: true,
+          version: 'v12.0'
         });
       };
-  (function(d, s, id) {
+     (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
         js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));
-
+  }
+  function countVisitor(){  
+    var n = localStorage.getItem('on_load_counter');
+    if (n === null) {
+        n = 0;
+    }
+    n++;
+    localStorage.setItem("on_load_counter", n);
+    document.getElementById('CounterVisitor').innerHTML = n;
+  }
   return (
     <div className={styles.container}> 
     {/* Styles.container là Home.module.css .container{ */}
@@ -51,17 +64,20 @@ const Home: NextPage = () => {
           </audio>
           </div>
         </div>
+        <div>  
+          <div id="fb-root"></div>
+          <div id="fb-customer-chat" className="fb-customerchat">
+          </div>
+        </div>
       </main>
 
       <footer className={styles.footer}>
-      <div id="fb-root"></div>
-      <div id="fb-customer-chat" className="fb-customerchat">
-      </div>
-
-    <script>
-     
-    </script>
-
+       <div>
+          <p>
+          Visitor this page : 
+          <span id="CounterVisitor"></span>
+          </p>
+        </div>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
