98ae7490b88ef814df8fe9825ae90d0503b5cef8
luatpt <luatphamdhv@gmail.com>
2022-02-01 15:23:08 +0700

    Fix bug deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,29 @@ import Button from '@material-ui/core/Button';
 
 const Home: NextPage = () => {
   useEffect(function onFirstMount(){
-   window?.addEventListener('click', function() { document.getElementById('player').play() }, { once: true });
+   window?.addEventListener('click', function() { document?.getElementById('player').play() }, { once: true });
   },[])
   function getGift(){
    alert("AHhh!");
   }
+
+  var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "103999694475699");
+      chatbox.setAttribute("attribution", "biz_inbox");
+       window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v12.0'
+        });
+      };
+  (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+
   return (
     <div className={styles.container}> 
     {/* Styles.container là Home.module.css .container{ */}
@@ -36,6 +54,14 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
+      <div id="fb-root"></div>
+      <div id="fb-customer-chat" className="fb-customerchat">
+      </div>
+
+    <script>
+     
+    </script>
+
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
